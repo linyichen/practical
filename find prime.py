690a9d465c364241d2bf63c645e87cb09dbbd79a
linyichen <lin.yichen@dhs.sg>
2011-02-23 09:41:45 +0800

    add hellow.py and find_prime.py

---
+++ find prime.py
@@ -0,0 +1,17 @@
+# find prime.py
+# to find the nth prime number efficiently
+
+def is_prime():
+''' Function to determine if a number is prime
+    Input: integer k
+    Output: true if k is prime, else false
+'''
+
+# main
+# get input n 
+n = int(input("Enter n: "))
+
+# find nth prime number
+
+# display result
+print (ans)
